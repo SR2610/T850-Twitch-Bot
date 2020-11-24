d96f2d06629d744228bb0ef884b64636bade7532
Stephen Ray <stephenray@sr2610.com>
2020-11-24 22:29:37 +0000

    Restructed some of the main js file

--- skynet.js
+++ skynet.js
@@ -16,56 +16,62 @@ const authProvider = new StaticAuthProvider(clientId, accessToken);
 const apiClient = new ApiClient({ authProvider });
 
 const pubSubClient = new PubSubClient();
-const userId = registerListener().then(()=>{getListener();});
 
+var userId;
 
-
-let client = new huejay.Client({
-  host: process.env.HUE_IP,
-  port: 80,               // Optional
-  username: process.env.HUE_USER, // Optional
-  timeout: 15000,            // Optional, timeout in milliseconds (15000 is the default)
-});
 
 //LIGHTS
 const GROUP_ID = 9, RED = 'GgVtnAjJXCzxZyy', BLUE = 'jbtuQnGc7sIYznl', GREEN = 'BAyhtEu6aSNvqKj', PINK = 'lEdhmxQVq9WKdRj', PURPLE = 'c4F8p8eB4CSgEXc', WHITE = 'DLa-2W2ecctaccs';
 
 //REWARD IDS
 const REWARD_LIGHTS_RED = '6b6161cb-8d58-4996-a9be-d85ae77c169e', REWARD_LIGHTS_BLUE = '768c1085-8e0e-47f9-ad26-540b844c85d0',
-      REWARD_LIGHTS_GREEN = 'ab01dbb0-aff5-495a-9a9f-9209cf7533c0', REWARD_LIGHTS_PINK = 'fc8fd880-56c8-4a49-831b-132b7882de6a',
-      REWARD_LIGHTS_PURPLE = 'a2f3bf4b-dd63-4732-b4eb-fde790fe78ff', REWARD_LIGHTS_WHITE = '8a019f0a-b6d3-4dac-a0a2-967e86c52d53',
-      REWARD_LIGHTS_OFF = '6f986e2e-e1b1-4d0b-8496-986091bef7be', REWARD_DAB = '8488c296-5eee-40a2-a4eb-eefec9a3d5c8';;
+  REWARD_LIGHTS_GREEN = 'ab01dbb0-aff5-495a-9a9f-9209cf7533c0', REWARD_LIGHTS_PINK = 'fc8fd880-56c8-4a49-831b-132b7882de6a',
+  REWARD_LIGHTS_PURPLE = 'a2f3bf4b-dd63-4732-b4eb-fde790fe78ff', REWARD_LIGHTS_WHITE = '8a019f0a-b6d3-4dac-a0a2-967e86c52d53',
+  REWARD_LIGHTS_OFF = '6f986e2e-e1b1-4d0b-8496-986091bef7be', REWARD_DAB = '8488c296-5eee-40a2-a4eb-eefec9a3d5c8';
 
-huejay.discover()
-  .then(bridges => {
-    for (let bridge of bridges) {
-      console.log(`"Hue Bridge Discovered: " ID: ${bridge.id}, IP: ${bridge.ip}`);
-    }
-  })
-  .catch(error => {
-    console.log(`An error occurred when finding hue bridges: ${error.message}`);
+
+init();
+
+function init() {
+
+  userId = registerListener().then(() => { getListener(); });
+
+  let client = new huejay.Client({
+    host: process.env.HUE_IP,
+    port: 80,
+    username: process.env.HUE_USER,
+    timeout: 15000,
   });
 
 
+  huejay.discover()
+    .then(bridges => {
+      for (let bridge of bridges) {
+        console.log(`Hue Bridge Discovered: ID: ${bridge.id}, IP: ${bridge.ip}`);
+      }
+    })
+    .catch(error => {
+      console.log(`An error occurred when finding hue bridges: ${error.message}`);
+    });
 
-obs.connect({
-  address: process.env.OBS_IP,
-  password: process.env.OBS_PASSWORD
-})
-  .then(() => {
-    console.log(`Connected and Authenticated with OBS!`);
-
+  obs.connect({
+    address: process.env.OBS_IP,
+    password: process.env.OBS_PASSWORD
   })
-  .catch(err => {
-    console.log("Unable to connect with OBS-Websocket.  Ensure that OBS is running first!")
-    //console.log(err); //Don't always need error logged.  It is usually just obs not running that causes it.
+    .then(() => {
+      console.log(`Connected and Authenticated with OBS!`);
+
+    })
+    .catch(err => {
+      console.log("Unable to connect with OBS-Websocket.  Ensure that OBS is running first!")
+      //console.log(err); //Don't always need error logged.  It is usually just obs not running that causes it.
+    });
+
+
+  obs.on('error', err => { //Catch other obs websocket errors
+    console.error('socket error:', err);
   });
-
-
-obs.on('error', err => { //Catch other obs websocket errors
-  console.error('socket error:', err);
-});
-
+}
 
 async function registerListener() {
   return await pubSubClient.registerUserListener(apiClient);
@@ -73,35 +79,34 @@ async function registerListener() {
 
 
 async function getListener() {
-  return  await pubSubClient.onRedemption(await pubSubClient.registerUserListener(apiClient), (message) => {
-      console.log("REWARD HAS BEEN REDEEMED");
-      switch (message.rewardId) {
-        case REWARD_LIGHTS_RED:
-          setLightToScene(RED);
-          break;
-        case REWARD_LIGHTS_BLUE:
-          setLightToScene(BLUE);
-          break;
-        case REWARD_LIGHTS_GREEN:
-          setLightToScene(GREEN);
-          break;
-        case REWARD_LIGHTS_PINK:
-          setLightToScene(PINK);
-          break;
-        case REWARD_LIGHTS_PURPLE:
-          setLightToScene(PURPLE);
-          break;
-        case REWARD_LIGHTS_WHITE:
-          setLightToScene(WHITE);
-          break
-        case REWARD_LIGHTS_OFF:
-          turnOffLights();
-          break;
-        case REWARD_DAB:
-          switchToDabCamera();
-          break
-  
-      }
+  return await pubSubClient.onRedemption(await pubSubClient.registerUserListener(apiClient), (message) => {
+    switch (message.rewardId) {
+      case REWARD_LIGHTS_RED:
+        setLightToScene(RED);
+        break;
+      case REWARD_LIGHTS_BLUE:
+        setLightToScene(BLUE);
+        break;
+      case REWARD_LIGHTS_GREEN:
+        setLightToScene(GREEN);
+        break;
+      case REWARD_LIGHTS_PINK:
+        setLightToScene(PINK);
+        break;
+      case REWARD_LIGHTS_PURPLE:
+        setLightToScene(PURPLE);
+        break;
+      case REWARD_LIGHTS_WHITE:
+        setLightToScene(WHITE);
+        break
+      case REWARD_LIGHTS_OFF:
+        turnOffLights();
+        break;
+      case REWARD_DAB:
+        switchToDabCamera();
+        break
+
+    }
   });
 }
 
@@ -135,7 +140,7 @@ function turnOffLights() {
       return client.groups.save(group);
     })
     .then(group => {
-     // console.log(`Group [${group.id}] was saved`);
+      // console.log(`Group [${group.id}] was saved`);
     })
     .catch(error => {
       console.log(error.stack);
@@ -156,7 +161,6 @@ async function switchToDabCamera() {
     'scene-name': 'DabCamera'
   });
   await sleep(4000)
-
   console.log((await sceneName).name);
   obs.send('SetCurrentScene', {
     'scene-name': (await sceneName).name
